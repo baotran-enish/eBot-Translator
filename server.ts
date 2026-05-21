972b7cb753ac724568d60d62fe7b297dbc3593aa
baotran-enish <bao.tran@enish.com>
2026-05-21 16:05:02 +0900

    feat: Listen on all interfaces by default
    Allows the server to be accessed from other devices on the local network, in addition to localhost.

--- server.ts
+++ server.ts
@@ -27,7 +27,7 @@ async function startServer() {
     });
   }
 
-  app.listen(PORT, "localhost", () => {
+  app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on http://localhost:${PORT}`);
   });
 }
